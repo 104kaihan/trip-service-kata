acf827de02325af14db249bb6d974e6ef794d745
kaihan <kaihan.chang@104.com.tw>
2022-04-18 02:10:26 +0800

    Extract isFriend

--- php/src/TripServiceKata/Trip/TripService.php
+++ php/src/TripServiceKata/Trip/TripService.php
@@ -23,17 +23,30 @@ class TripService
         $loggedUser = $userSessionHelp->getLoggedUser();
 
         if ($loggedUser !== null) {
-            $isFriend = false;
-            foreach ($user->getFriends() as $friend) {
-                if ($friend == $loggedUser) {
-                    $isFriend = true;
-                    break;
-                }
-            }
+            $isFriend = $this->isFriend($user, $loggedUser);
 
             return $isFriend ? $this->tripDAOHelp->findTripsByUser($user) : [];
         }
 
         throw new UserNotLoggedInException();
+    }
+
+    /**
+     * @param User $user
+     * @param User $loggedUser
+     *
+     * @return bool
+     */
+    public function isFriend(User $user, User $loggedUser): bool
+    {
+        $isFriend = false;
+        foreach ($user->getFriends() as $friend) {
+            if ($friend == $loggedUser) {
+                $isFriend = true;
+                break;
+            }
+        }
+
+        return $isFriend;
     }
 }
